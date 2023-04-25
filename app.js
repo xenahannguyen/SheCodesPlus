092cdc8ccc487a211aeab0d1113e6841750abf63
xenahannguyen <90170175+xenahannguyen@users.noreply.github.com>
2023-04-24 19:36:46 -0500

    New JS File (week 3 hw)
    JS file: prompted users to search for desired city's weather. file included info properties of 5 specific cities. if searched city was not in code or is user had not responded then page would alert accordingly.
    
    Modified the CSS to center the card group and main section.
    
    Added the new JS file to HTML.
    <script src=app.js></script>

---
+++ app.js
@@ -0,0 +1,38 @@
+let city = prompt("Enter a city");
+
+let weather = {
+    paris: {
+      temp: 19.7,
+      humidity: 80
+    },
+    tokyo: {
+      temp: 17.3,
+      humidity: 50
+    },
+    lisbon: {
+      temp: 30.2,
+      humidity: 20
+    },
+    "san francisco": {
+      temp: 20.9,
+      humidity: 100
+    },
+    oslo: {
+      temp: -5,
+      humidity: 20
+    }
+  };
+
+
+if (weather.hasOwnProperty(city)) {
+let cityTemp = Math.round(weather[city].temp);
+let cityHumid = weather[city].humidity;
+let fahTemp = Math.round((weather[city].temp) * 9 / 5 + 32);
+alert(`It is currently ${cityTemp}°C (${fahTemp}°F) in ${city} with a humidity of ${cityHumid}%.`)
+} 
+if (city.length === 0) {
+    alert("You forgot to search for a city, silly!");
+}else {
+    alert("Sorry we don't have data for that city yet!😢");
+}
+
